236e93d75381671e7b6338ef803f13a1df9115f4
leedohyung28 <dhleee0123@gmail.com>
2024-08-12 16:32:17 +0900

    0.0.1 update

---
+++ index.js
@@ -0,0 +1,4 @@
+export { checkedArr } from "./src/checkedArr";
+export { findOddNums } from "./src/findOddNums";
+export { findEvenNums } from "./src/findEvenNums";
+export { capitalize } from "./src/capitalize";
